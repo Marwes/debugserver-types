67f80fd7c48fee6d9c7be7edee84cae0ea2d83f5
Markus Westerlind <marwes91@gmail.com>
2016-10-31 23:45:46 +0100

    feat: Initial commit

---
+++ build.rs
@@ -0,0 +1,31 @@
+extern crate json_schema;
+
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
+fn main() {
+    let schema = "src/schema.json";
+    println!("cargo:rerun-if-changed={}", schema);
+    let src = Path::new(schema);
+
+    let mut file = File::open(src).unwrap();
+    let mut input = String::new();
+    file.read_to_string(&mut input).unwrap();
+
+    let output = json_schema::generate(None, &input).unwrap();
+    let dst = Path::new("src/lib.rs");
+
+    let mut file = File::create(dst).unwrap();
+    file.write_all(br#"
+#![feature(proc_macro)]
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate serde;
+
+"#)
+        .unwrap();
+    file.write_all(output.as_bytes()).unwrap();
+}
